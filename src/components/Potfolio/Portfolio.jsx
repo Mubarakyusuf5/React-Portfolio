40eb30523a5a760df328f4775a242d24402e8b76
Mubarak Yusuf <yusufmubarak460@gmail.com>
2024-04-09 04:14:42 +0100

    'progress'

--- src/components/Potfolio/Portfolio.jsx
+++ src/components/Potfolio/Portfolio.jsx
@@ -6,6 +6,12 @@ export const Portfolio = () => {
 <section className='portfolio' id='portfolio' >
 <h5>Recent Projects</h5>
 <h2>My Portfolio</h2>
+<div className='portfolio-con'>
+  <h1>
+    Under Construction
+  </h1>
+  </div>
+
 </section>
     </>
   )

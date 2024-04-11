7f06542c34c7b047077f58a05e3199f583b6cc76
Mubarak Yusuf <yusufmubarak460@gmail.com>
2024-04-11 10:31:16 +0100

    'activ'

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,22 +4,32 @@ import {
     faBars,
     faTimes
   } from "@fortawesome/free-solid-svg-icons";
-
+import {Link} from 'react-scroll'
 import './Navbar.css'
 
 export const Navbar = () => {
     const [isRes, setIsRes] = useState(false)
     const [navbar, setIsNavbar] = useState(false)
     const links =[
-        {id:1, name:'Home', url:'#home'},
-        {id:2, name:'About', url:'#about'},
-        {id:3, name:'Services', url:'#service'},
-        {id:4, name:'Portfolio', url:'#portfolio'},
-        {id:5, name:'Contact', url:'#contact'},
+        {id:1, name:'Home', url:'home'},
+        {id:2, name:'About', url:'about'},
+        {id:3, name:'Services', url:'service'},
+        {id:4, name:'Portfolio', url:'portfolio'},
+        {id:5, name:'Contact', url:'contact'},
     ]
 
-    const place = links.map(link => (
-        <li key={link.id} ><a href={link.url}>{link.name}</a></li>
+    const place = links.map(({id, name, url}) => (
+        <li key={id} >
+        <Link 
+        activeClass="activ" 
+        to={url} 
+        spy={true} 
+        smooth={true} 
+        offset={-30} 
+        duration={500} 
+      >
+        {name}
+      </Link></li>
     ))
 const toggle = ()=> {
     setIsRes(preRes => !preRes)
